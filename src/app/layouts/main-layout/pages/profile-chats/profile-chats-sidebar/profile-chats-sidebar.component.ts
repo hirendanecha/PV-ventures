ba00bdb4ed8400bb48b77b154ba449bea076781e
prashant <ppo.opash@gmail.com>
2024-06-14 17:20:28 +0530

    Add google link change PM UI and fixed User Show

--- src/app/layouts/main-layout/pages/profile-chats/profile-chats-sidebar/profile-chats-sidebar.component.ts
+++ src/app/layouts/main-layout/pages/profile-chats/profile-chats-sidebar/profile-chats-sidebar.component.ts
@@ -14,6 +14,7 @@ import {
   NgbActiveOffcanvas,
   NgbDropdown,
   NgbModal,
+  NgbOffcanvas,
 } from '@ng-bootstrap/ng-bootstrap';
 import { SocketService } from 'src/app/@shared/services/socket.service';
 import { SharedService } from 'src/app/@shared/services/shared.service';
@@ -42,6 +43,7 @@ export class ProfileChatsSidebarComponent
   userSearchNgbDropdown: NgbDropdown;
   searchText = '';
   userList: any = [];
+  backCanvas: boolean = true;
   profileId: number;
   selectedChatUser: any;
 
@@ -65,7 +67,8 @@ export class ProfileChatsSidebarComponent
     private router: Router,
     private toasterService: ToastService,
     public encryptDecryptService: EncryptDecryptService,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private activeCanvas: NgbOffcanvas,
   ) {
     this.profileId = +localStorage.getItem('profileId');
     const notificationSound =
@@ -96,6 +99,7 @@ export class ProfileChatsSidebarComponent
     this.socketService.connect();
     this.getChatList();
     this.getGroupList();
+    this.backCanvas =this.activeCanvas.hasOpenOffcanvas();
   }
 
   ngAfterViewInit(): void {
